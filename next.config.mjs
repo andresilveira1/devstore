4ca89b6e005eb01508b26485e9c04c6cfd999c96
André Silveira <andre.silveira1silva@gmail.com>
2024-10-22 15:08:11 -0300

    chore: add external Image config and extends Tailwind props(font, gridtr).

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+        pathname: '/andresilveira1.png',
+      },
+    ],
+  },
+}
 
 export default nextConfig
