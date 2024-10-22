b0f0bd694d63a59a1f578bef5bc9bd64c3c0f4c9
André Silveira <andre.silveira1silva@gmail.com>
2024-10-22 15:05:09 -0300

    chore: create API connection and Product Interface.

---
+++ src/env.ts
@@ -0,0 +1,18 @@
+import * as zod from 'zod'
+
+const envSchema = zod.object({
+  NEXT_PUBLIC_API_BASE_URL: zod.string().url(),
+})
+
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (!parsedEnv.success) {
+  console.error(
+    'Invalid environment variables',
+    parsedEnv.error.flatten().fieldErrors,
+  )
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = parsedEnv.data
