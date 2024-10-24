ae5509fc76cfee9b75b2b1c5f170958f7fcdcf9c
André Silveira <andre.silveira1silva@gmail.com>
2024-10-24 17:32:12 -0300

    chore: add E2E tests with Cypress.

---
+++ cypress/support/commands.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable {
+    searchByQuery(query: string): Chainable<void>
+  }
+}
+
+// -- This is a parent command --
+Cypress.Commands.add('searchByQuery', (query: string) => {
+  cy.visit('/')
+
+  cy.get('input[name=q]').type(query).parent('form').submit()
+})
