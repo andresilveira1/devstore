4f6ae05517b2e4ae3451b04e58f420f8b126853a
André Silveira <andre.silveira1silva@gmail.com>
2024-10-25 12:29:51 -0300

    chore: add OG Image.

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import * as zod from 'zod'
 
 const envSchema = zod.object({
   NEXT_PUBLIC_API_BASE_URL: zod.string().url(),
-  // APP_URL: zod.string().url(),
+  APP_URL: zod.string().url(),
 })
 
 const parsedEnv = envSchema.safeParse(process.env)
