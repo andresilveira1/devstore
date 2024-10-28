26a98aff09baf98b2cbf43b05d639e2b51440db5
André Silveira <andre.silveira1silva@gmail.com>
2024-10-28 18:19:12 -0300

    refactor: rename product folder to products

--- src/app/api/products/search/route.ts
+++ src/app/api/products/search/route.ts
@@ -4,6 +4,8 @@ import type { NextRequest } from 'next/server'
 import data from '../data.json'
 
 export async function GET(request: NextRequest) {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+
   const { searchParams } = request.nextUrl
 
   const query = zod.string().parse(searchParams.get('q'))
